ee7cd7ce8cccdf00880f9969ed6deac7a60af374
Evgen-Mutagen <jeka190@mail.ru>
2022-12-14 21:58:47 +0300

    added:update, delete, description

--- src/main/java/com/github/app/mongo/controller/UserController.java
+++ src/main/java/com/github/app/mongo/controller/UserController.java
@@ -9,8 +9,10 @@ import io.swagger.v3.oas.annotations.tags.Tag;
 import lombok.AllArgsConstructor;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
+import org.springframework.http.HttpStatus;
 import org.springframework.web.bind.annotation.*;
 
+import javax.validation.Valid;
 import java.util.List;
 import java.util.Optional;
 
@@ -33,22 +35,50 @@ public class UserController {
     }
 
     @RequestMapping(value = "/create", method = RequestMethod.POST)
-    public User addNewUsers(@RequestBody User user) {
+    @Operation(summary = "Добавить нового пользователя")
+    public User addNewUsers(@Valid @RequestBody User user) {
         LOG.info("Saving user.");
         return userRepository.save(user);
     }
 
+    @RequestMapping(value = "/update/{userId}", method = RequestMethod.PUT)
+    @ResponseStatus(HttpStatus.NO_CONTENT)
+    @Operation(summary = "Обновить информацию о пользователе")
+    public void update(@Parameter(description = "id пользователя") @PathVariable String userId, @Valid @RequestBody User user) {
+        LOG.info("update {} with id={}", user, userId);
+        Optional<User> oldUser = userRepository.findById(userId);
+        if (oldUser.isPresent()) {
+            User updatedUser = oldUser.get();
+            updatedUser.setUserId(user.getUserId());
+            updatedUser.setFirstName(user.getFirstName());
+            updatedUser.setLastName(user.getLastName());
+            updatedUser.setEmail(user.getEmail());
+            updatedUser.setPhoneNumber(user.getPhoneNumber());
+            userRepository.save(updatedUser);
+        }
+    }
+
+
     @RequestMapping(value = "/id/{userId}", method = RequestMethod.GET)
     @Operation(summary = "Получить информацию о пользователе по его id")
-    public Optional<User> getUserById(@Parameter(description = "id пользователя")@PathVariable String userId) {
+    public Optional<User> getUserById(@Parameter(description = "id пользователя") @PathVariable String userId) {
         LOG.info("Getting user with ID: {}.", userId);
         return userRepository.findById(userId);
     }
 
     @RequestMapping(value = "/name/{firstName}", method = RequestMethod.GET)
-    public List<User> getUserByName(@PathVariable String firstName) {
+    @Operation(summary = "Получить информацию о пользователе по его имени")
+    public List<User> getUserByName(@Parameter(description = "имя пользователя") @PathVariable String firstName) {
         LOG.info("Getting user with firstName: {}.", firstName);
         return userRepository.findByFirstName(firstName);
+    }
+
+    @DeleteMapping(value = "/{userId}")
+    @ResponseStatus(HttpStatus.NO_CONTENT)
+    @Operation(summary = "Удалить пользователя по его по его id")
+    public void delete(@PathVariable String userId) {
+        LOG.info("delete menu with id={}", userId);
+        userRepository.deleteById(userId);
     }
 
     public void initializationUsers() {
--- src/main/java/com/github/app/mongo/repository/UserRepository.java
+++ src/main/java/com/github/app/mongo/repository/UserRepository.java
@@ -2,7 +2,6 @@ package com.github.app.mongo.repository;
 
 import com.github.app.mongo.model.User;
 import org.springframework.data.mongodb.repository.MongoRepository;
-import org.springframework.data.mongodb.repository.Query;
 import org.springframework.stereotype.Repository;
 
 import java.util.List;
